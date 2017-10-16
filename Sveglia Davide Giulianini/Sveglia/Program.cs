b378d3e1202e0f10bfad5b183b5c990f6a675d70
GIULIANINI DAVIDE <DAVIDE.GIULIANINI@itis.ce>
2017-10-16 09:48:47 +0200

    promise commit

---
+++ Sveglia Davide Giulianini/Sveglia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AlarmClock
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IAlarmClock sveglia = new AlarmClock();
+
+            sveglia.PowerOn();
+            sveglia.Time = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+            sveglia.AlarmTime = new DateTime(2017, 10, 10, 11, 21, 30).ToString("yyyy/MM/dd HH:mm:ss");
+            Console.WriteLine("premi un tasto per terminare");
+            Console.ReadLine();
+
+            sveglia.PowerOff();
+        }
+    }
+}
